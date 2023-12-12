96902009c91ea7550d89aec81df8a4ebed21399d
Jeisson Chacon <Jeissonchaconjp@gmail.com>
2023-12-12 18:11:56 -0500

    Adding dependencies

---
+++ src/main/java/com/example/springtotal/SpringTotalApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTotalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTotalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtotal/SpringTotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTotalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
